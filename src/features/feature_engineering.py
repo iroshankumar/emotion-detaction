3d1fe3ec710bd338ad70a618d16888ced09488cd
Roshan  Kumar <roshankumar@Roshans-MacBook-Pro.local>
2026-03-20 15:41:09 +0530

    removing matplotlib import

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import os
 
 from sklearn.feature_extraction.text import TfidfVectorizer
--- src/models/model_building.py
+++ src/models/model_building.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import pickle
 from sklearn.ensemble import GradientBoostingClassifier
 import yaml
